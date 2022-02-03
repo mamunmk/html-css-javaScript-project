7743b15c52332d9a07ab41d534e92601fb2537f9
Mamun <guest.batworld@gmail.com>
2022-02-04 01:43:27 +0600

    Blog section design done

---
+++ Personal Portfolio website using HTML and CSS/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".single-item").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    rtl: false,
+  });
+});
